222c9fa7864a58a5aca627d2748071e30487ecdc
scjeong <scjeong@raoncorp.com>
2024-04-01 17:12:35 +0900

    1차 커밋

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    asd
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div id="main">
+      <div className="main_bg">
+        <div className="main_body">
+          <div className="left"></div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
